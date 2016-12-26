35dcd3800fbc40af6eea320cfe081844710959e1
Daniel Araujo <danielaraujos@live.com>
2016-12-26 11:50:23 -0200

    Comecando a arrumar o json

--- config/routes.php
+++ config/routes.php
@@ -23,24 +23,7 @@ use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
 
-/**
- * The default class to use for all routes
- *
- * The following route classes are supplied with CakePHP and are appropriate
- * to set as the default:
- *
- * - Route
- * - InflectedRoute
- * - DashedRoute
- *
- * If no call is made to `Router::defaultRouteClass()`, the class used is
- * `Route` (`Cake\Routing\Route\Route`)
- *
- * Note that `Route` does not do any inflections on URLs which will result in
- * inconsistently cased URLs when used with `:plugin`, `:controller` and
- * `:action` markers.
- *
- */
+
 Router::defaultRouteClass(DashedRoute::class);
 
 Router::scope('/', function (RouteBuilder $routes) {
@@ -49,8 +32,21 @@ Router::scope('/', function (RouteBuilder $routes) {
 });
 
 Router::scope('/api/v1', function (RouteBuilder $routes) {
+    //Adicionando Extensao do Json
     $routes->extensions(['Json']);
+    //Sobre
+    $routes->resources('Abouts');
 
+    $routes->resources('Benefits');                      //Beneficios
+    $routes->resources('Buildings');         //Edificios
+    $routes->resources('Calendars');         //Calendarios
+    $routes->resources('CategoryManagements');         //Edificios
+    $routes->resources('Buildings');         //Edificios
+    $routes->resources('Buildings');         //Edificios
+    $routes->resources('Buildings');         //Edificios
+    $routes->resources('Buildings');         //Edificios
+    $routes->resources('Buildings');         //Edificios
+    $routes->resources('Buildings');         //Edificios
     $routes->fallbacks('DashedRoute');
 });
 
@@ -59,8 +55,4 @@ Router::prefix('admin',function ($routes){
     $routes->fallbacks('DashedRoute');
 });
 
-/**
- * Load all plugin routes.  See the Plugin documentation on
- * how to customize the loading of plugin routes.
- */
 Plugin::routes();
